080377324dd0ddf564f76a3d4612a0c7c4ffda5d
creativedesignseo <creativedesignseo@gmail.com>
2025-08-09 20:28:39 +0200

    Actualizacion y limpiezatest mt2t

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
           Comparte el precio de
           <br />
           <span className="bg-gradient-to-r from-[#059669] to-[#10B981] bg-clip-text text-transparent">
-            tus suscripciones hoy mismo
+            tus suscripciones hoy misom nota
           </span>
         </h1>
 
